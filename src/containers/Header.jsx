a3f36fc575618ff685a01ba09cc7e2f8b4ddc968
deddyps <haicoders12@gmail.com>
2023-12-15 14:20:40 +0700

    add setup tailwind css

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -89,7 +89,7 @@ export default function Header() {
           >
             <img
               loading="lazy"
-              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/53d8af31e6ef65393d12fb910d1a94d2038972ee16066326db946515a4806c5f?apiKey=7ef2d401d2464e0bb0e4708e7eee43f9&width=100 100w,"
               style={{
                 aspectRatio: "4.18",
                 objectFit: "contain",
