3ba7434cdd66c72611557f4cf589bbc005b6efeb
Aditya Perwira Joan Dwitama <aditjd@gmail.com>
2023-12-20 11:32:20 +0800

    migrate service using axios

--- src/services/Product.jsx
+++ src/services/Product.jsx
@@ -1,31 +1,31 @@
+import axios from 'axios';
 import { config } from "../helper/config";
 
-export const apiProduct = async(method, path, body) => {
+export const apiProduct = async (method, path, body) => {
   try {
     let url = `${config.API_URL}/${path}`;
-    const options = {
+    const axiosConfig = {
       method,
       headers: {
         'Content-Type': 'application/json',
-      }
-    }
+      },
+    };
 
     if (method.toUpperCase() === 'GET' && body) {
       const queryParams = new URLSearchParams(body);
       url = `${url}?${queryParams.toString()}`;
     } else if (body) {
-      options.body = JSON.stringify(body);
+      axiosConfig.data = body;
     }
 
-    const response = await fetch(url, options);
-    const result = await response.json()
-    return result
+    const response = await axios(url, axiosConfig);
+    return response.data;
   } catch (error) {
     console.error(`Error api ${path}:`, error);
     return {
       "resultCode": 500,
       "status": "ERROR",
-      "message": `Error on api ${path}` 
+      "message": `Error on api ${path}`
     };
   }
-}
+};
