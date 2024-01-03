586854bb6a733ee79e422220dbd705a7754e9cfb
Aditya Perwira Joan Dwitama <aditjd@gmail.com>
2024-01-03 14:58:49 +0800

    update FormEmailLogin.jsx

--- src/components/Auth/ModalLoginAndRegister/Login/FormEmailLogin.jsx
+++ src/components/Auth/ModalLoginAndRegister/Login/FormEmailLogin.jsx
@@ -1,6 +1,4 @@
 import PropTypes from "prop-types";
-import { RenderInputFieldCustom } from "../../../InputFieldCustom";
-import { Trans } from "react-i18next";
 import { FormEmail } from "../FormInput/FormEmail";
 
 export const RenderEmailLogin = ({ setEmailField }) => {
