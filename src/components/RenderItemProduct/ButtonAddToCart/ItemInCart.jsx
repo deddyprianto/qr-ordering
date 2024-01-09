9837a32dea2dd976ce36c4d894e22dd6121a2a24
Aditya Perwira Joan Dwitama <aditjd@gmail.com>
2024-01-09 20:09:40 +0800

    apply color mapping in RenderButtonItemInCart

--- src/components/RenderItemProduct/ButtonAddToCart/ItemInCart.jsx
+++ src/components/RenderItemProduct/ButtonAddToCart/ItemInCart.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { IconMinus, IconPlus } from "../../../assets/svgIcon"
 import PropTypes from 'prop-types'
 
@@ -7,15 +8,16 @@ export const RenderButtonItemInCart = ({
   cartLineID,
   handleClickButtonAdd
 }) => {
+  const theme = useSelector((state) => state.dataSlice.theme);
   const handleClickChangeQty = (qty) => {
-    console.log(cartLineID)
     handleClickButtonAdd(qty, cartLineID)
   }
   return (
     <div className="items-stretch flex gap-1 mt-2"
       style={{filter: isLoading ? "blur(1px)" : "",}}
     >
-      <div className="justify-center items-center bg-[#FF4782] flex flex-col px-3 py-1 rounded-lg"
+      <div className="justify-center items-center flex flex-col px-3 py-1 rounded-lg"
+        style={{backgroundColor: theme.secondary}}
         onClick={()=>handleClickChangeQty(qtyInCart-1)}
       >
         <IconMinus width="16"/>
@@ -23,7 +25,8 @@ export const RenderButtonItemInCart = ({
       <span className="text-gray-700 text-center text-base font-bold leading-6 whitespace-nowrap justify-center items-stretch bg-zinc-300 px-7 py-1 rounded-lg">
         {qtyInCart}
       </span>
-      <div className="justify-center items-center bg-[#FF4782] flex flex-col px-3 py-1 rounded-lg"
+      <div className="justify-center items-center flex flex-col px-3 py-1 rounded-lg"
+        style={{backgroundColor: theme.secondary}}
         onClick={()=>handleClickChangeQty(qtyInCart+1)}
       >
         <IconPlus width="16"/>
