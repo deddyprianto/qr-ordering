59e31bfddde4e8c4ebdcf8f61b7fa569a1e38198
Aditya Perwira Joan Dwitama <aditjd@gmail.com>
2024-01-25 13:01:03 +0800

    loader when submit payment

--- src/pages/Payment/Stripe/CheckoutForm.jsx
+++ src/pages/Payment/Stripe/CheckoutForm.jsx
@@ -1,14 +1,17 @@
 import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { RenderButtonSubmit } from "./ButtonSubmit";
 import { useEdgeSnack } from "../../../components/EdgeSnack/utils/useEdgeSnack";
+import { useState } from "react";
 
 export const RenderCheckoutPage = () => {
   const stripe = useStripe();
   const elements = useElements();
   const toast = useEdgeSnack();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async(e) => {
     e.preventDefault();
+    setLoading(true);
     const {error} = await stripe.confirmPayment({
       elements,
       confirmParams: {
@@ -16,8 +19,10 @@ export const RenderCheckoutPage = () => {
       },
     });
 
-    if (error)
-      toast.open(error.message, 'error');
+    if (error){
+      setLoading(false);
+      toast.open(error.message, 'error');	      
+    }
   }
   return (
     <form id={"payment-form"}>
@@ -28,7 +33,7 @@ export const RenderCheckoutPage = () => {
             },
           },
         }}/>
-      <RenderButtonSubmit handleSubmit={handleSubmit}/>
+      <RenderButtonSubmit handleSubmit={handleSubmit} loading={loading}/>
     </form>
   )
 }
