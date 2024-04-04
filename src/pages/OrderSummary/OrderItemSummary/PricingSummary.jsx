aa818e87a4d7e2abdc0837caacd6d46d49279819
deddyps <haicoders12@gmail.com>
2024-04-04 16:13:49 +0700

    add surcharge on the order summary

--- src/pages/OrderSummary/OrderItemSummary/PricingSummary.jsx
+++ src/pages/OrderSummary/OrderItemSummary/PricingSummary.jsx
@@ -10,10 +10,10 @@ export const RenderPricingSummary = ({ order }) => {
           <Trans i18nKey={"subtotal"} />
         </div>
         <div className="text-gray-700 text-right text-base font-bold leading-6 whitespace-nowrap">
-          $ {numberFormatter(order.grossAmount)}
+          $ {numberFormatter(order?.subtotalSummary?.GROSS)}
         </div>
       </div>
-      <div className="items-stretch flex justify-between gap-4 mt-4 px-4">
+      <div className="items-stretch flex justify-between gap-4 mt-1 px-4">
         <div className="justify-center text-gray-700 text-sm font-medium leading-5 tracking-wide grow whitespace-nowrap">
           <Trans i18nKey={"discount"} />
         </div>
@@ -21,8 +21,23 @@ export const RenderPricingSummary = ({ order }) => {
           ($ {numberFormatter(order?.subtotalSummary?.LINE_DISC)})
         </div>
       </div>
+      {order?.subtotals?.map((sc) => {
+        return (
+          <div
+            className="items-stretch flex justify-between gap-4 mt-1 px-4"
+            key={sc.refNo}
+          >
+            <div className="justify-center text-gray-700 text-sm font-medium leading-5 tracking-wide grow whitespace-nowrap">
+              {sc?.subtotalNameDisplayed}
+            </div>
+            <div className="text-gray-700 text-right text-base font-bold leading-6 whitespace-nowrap">
+              $ {sc?.subtotalAmount}
+            </div>
+          </div>
+        );
+      })}
       {order?.excGSTAmount > 0 && (
-        <div className="items-stretch flex justify-between gap-4 mt-2 px-4">
+        <div className="items-stretch flex justify-between gap-4 mt-4 px-4">
           <div className="justify-center text-gray-700 text-sm font-medium leading-5 tracking-wide grow whitespace-nowrap">
             GST {order?.gstRate}%
           </div>
@@ -31,18 +46,23 @@ export const RenderPricingSummary = ({ order }) => {
           </div>
         </div>
       )}
-
-      <div className="bg-zinc-300 min-h-[1px] w-full mt-4" />
+      <div
+        className={
+          order?.excGSTAmount > 0
+            ? "bg-zinc-300 min-h-[1px] w-full mt-1"
+            : "bg-zinc-300 min-h-[1px] w-full mt-4"
+        }
+      />
       <div className="justify-between flex w-full gap-5 mt-4 px-4 items-start">
         <div className="text-gray-700 text-sm font-medium leading-5 tracking-wide">
           <Trans i18nKey={"total_payment"} />
         </div>
         <div className="text-emerald-800 text-base font-bold leading-6 self-stretch">
-          $ {numberFormatter(order.nettAmount)}
+          $ {numberFormatter(order?.subtotalSummary?.NETT)}
         </div>
       </div>
       {order?.incGSTAmount > 0 && (
-        <div className="items-stretch flex justify-between gap-4 mt-2 px-4">
+        <div className="items-stretch flex justify-between gap-4 mt-1 px-4">
           <div className="justify-center text-gray-700 text-sm font-medium leading-5 tracking-wide grow whitespace-nowrap">
             GST (inc) {order?.gstRate}%
           </div>
