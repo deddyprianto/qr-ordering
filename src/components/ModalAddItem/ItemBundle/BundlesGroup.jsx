1c6327c00e0d3ff5166dcce173520699aa682e8e
deddyps <haicoders12@gmail.com>
2024-05-10 16:33:09 +0700

    remove unused code

--- src/components/ModalAddItem/ItemBundle/BundlesGroup.jsx
+++ src/components/ModalAddItem/ItemBundle/BundlesGroup.jsx
@@ -32,8 +32,6 @@ export const RenderBundleGroup = ({
     }
   }, [bundleList, groupIdx]);
 
-  console.log("isExpanded =>", isExpanded);
-  
   const expandBundeItem = () => {
     if (!isExpanded) return null;
     else
